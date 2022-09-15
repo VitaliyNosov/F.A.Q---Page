1301c4aab3a870ff28b5624bcb6bbe8949494b81
VitaliyNosov <webdevelorestudio2017@gmail.com>
2022-09-15 17:00:12 +0300

    add vanila js script

--- js/common.js
+++ js/common.js
@@ -1,17 +1,55 @@
 console.log('script');
 
-$(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+// $(document).ready(function(){
+// 	$("#menu").on("click","a", function (event) {
+// 		//отменяем стандартную обработку нажатия по ссылке
+// 		event.preventDefault();
 
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
+// 		//забираем идентификатор бока с атрибута href
+// 		var id  = $(this).attr('href'),
 
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
+// 		//узнаем высоту от начала страницы до блока на который ссылается якорь
+// 			top = $(id).offset().top;
 		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+// 		//анимируем переход на расстояние - top за 1500 мс
+// 		$('body,html').animate({scrollTop: top}, 1500);
+// 	});
+// });
+
+
+// vanila js
+
+
+// собираем все якоря; устанавливаем время анимации и количество кадров
+const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+      animationTime = 1400,
+      framesCount = 40;
+
+anchors.forEach(function(item) {
+  // каждому якорю присваиваем обработчик события
+  item.addEventListener('click', function(e) {
+    // убираем стандартное поведение
+    e.preventDefault();
+    
+    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+    
+    // запускаем интервал, в котором
+    let scroller = setInterval(function() {
+      // считаем на сколько скроллить за 1 такт
+      let scrollBy = coordY / framesCount;
+      
+      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+      // и дно страницы не достигнуто
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        // то скроллим на к-во пикселей, которое соответствует одному такту
+        window.scrollBy(0, scrollBy);
+      } else {
+        // иначе добираемся до элемента и выходим из интервала
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    // время интервала равняется частному от времени анимации и к-ва кадров
+    }, animationTime / framesCount);
+  });
 });
